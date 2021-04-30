e2d30e7af98661c65230f8e5407d3d9c8dc0c1f8
Jules <bigaston1@gmail.com>
2021-04-30 09:16:23 +0200

    Changement nom de la sheet

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const publicSpreadsheetUrl =
   "https://docs.google.com/spreadsheets/d/1cmInVkZw_Qzfbev_v5DQnI8QT6_GBrLa1mQyvyxARm8/edit?usp=sharing"; // change this to your own URL
 const categoryStartNum = 3; // let the program know where the categoy begins on the spreadsheet column. Default value is 3.
-const sheetName = "Sheet1"; // this has to match your google doc sheet name
+const sheetName = "Liens"; // this has to match your google doc sheet name
 const punctuation = ": "; // this changes the punctuation between the title and the description. In most cases you'd want to use "," or "-" or ":"
 
 var dataTable = [];
